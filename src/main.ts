df128a41041546325fb714b1e0ec5e8b64612b28
dpryan79 <dpryan79@gmail.com>
2019-09-10 16:09:43 +0200

    foo

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,28 @@
-const core = require('@actions/core');
+//const core = require('@actions/core');
 const exec = require('@actions/exec');
 const tc = require('@actions/tool-cache');
 const io = require('@actions/io');
 
 async function run() {
-    const os = core.getInput('os', {required: true});
-    var home = "/home/runner";
+    //const os = core.getInput('os', {required: true});
+
+    // This should all be cached!
+    if(process.platform == "linux") {
+        var URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh";
+        var home = "/home/runner";
+    } else {
+        var URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh";
+        var home = "/Users/home/runner";
+    }
 
     // Otherwise conda can't install for some reason
     await io.mkdirP(home.concat('/.conda'));
 
-    // This should all be cached!
-    //if(process.platform == "linux") {
-    if(os == "linux") {
-        var URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh";
-    } else {
-        var URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh";
-    }
     const installerLocation = await tc.downloadTool(URL);
 
     await exec.exec("bash", [installerLocation, "-b", "-p", home.concat("/miniconda")]);
 
-    //core.addPath(home.concat("/miniconda/bin"))
+    // core.addPath(home.concat("/miniconda/bin"))
 
     await exec.exec(home.concat("/miniconda/bin/conda"), ["config", "--set", "always_yes", "yes"]);
 
